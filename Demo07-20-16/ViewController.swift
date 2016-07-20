42c163e342415a8291707db7928d1bb09c3c641e
kiwiecho <shuting@shutings-macbook-pro.local>
2016-07-20 11:09:26 -0500

    Initial Commit

---
+++ Demo07-20-16/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo07-20-16
+//
+//  Created by Shuting lang on 7/20/16.
+//  Copyright © 2016 Shutinglang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
